e7851dc25ff67d2e10336359f3d6c9ebf75f8eb7
jishouheng <jishouheng@hymson.com>
2020-02-26 11:23:36 +0800

    modify jump7.py

--- jump7.py
+++ jump7.py
@@ -1,3 +1,11 @@
-for a in range(1:100):
-	if a % 7 != 0:
-		print(a)
+for a in range(1,101):
+  if a%7 != 0:
+    c = str(a)
+    d = 1
+    for b in c:
+      if int(b) == 7:
+        d = 0
+    if d == 1:
+      print(a)
+
+
